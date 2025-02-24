c4880396e6abcc45a74c382eceba030a3df6ae8c
shaik-adnan1 <yousufwrk0@gmail.com>
2025-02-24 18:16:29 +0530

    Annotations

---
+++ index.ts
@@ -0,0 +1,33 @@
+type Person = {
+  name: string;
+};
+
+const person: Person = {
+  name: "Shaik",
+};
+
+// Annotation -
+/**
+ *
+ * Annotation are used to specify the data type of a variable,
+ * parameter, function return value and other type values.
+ *
+ */
+
+// -- Annotating
+// let myValL: type = value
+
+// -------- String ---------
+let myName: string = "Adnan Shaik";
+console.log(myName);
+myName = "Shaik yousuf Adnan";
+console.log(myName);
+
+// -------- Number ---------
+
+let favNumber: number = 7;
+console.log(favNumber);
+
+// -------- Boolean ---------
+let is_premium: boolean = true;
+console.log(is_premium);
